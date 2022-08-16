871e5b7c36f89251465fdcd2ac4e91d56a1391c1
ygyzy <70911240+ygyzy@users.noreply.github.com>
2022-08-17 01:17:57 +0800

    Create index.php

---
+++ biu/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>TVBoxosc魔改版-FREETVBOX.ML-关注微信公众号【晴园】</title>
+<meta http-equiv="Content-Language" content="zh-CN">
+<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=gb2312">
+
+<meta http-equiv="refresh" content="0.01;url=https://link.jscdn.cn/lanzou/aHR0cHM6Ly9iaXViaXVoZC5sYW56b3V0LmNvbS9pbDZtajA5Zjc0emc.apk">
+
+
+
+</head>
+<body>
+</body>
+</html>
